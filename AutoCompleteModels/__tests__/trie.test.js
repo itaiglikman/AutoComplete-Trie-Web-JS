bd43d88724a37f1fd8d420c0808e75ea95a110bc
itaiglikman <itaig1998@gmail.com>
2025-07-21 21:56:19 +0300

    feat: implement findWord test

--- AutoCompleteModels/__tests__/trie.test.js
+++ AutoCompleteModels/__tests__/trie.test.js
@@ -1,19 +1,44 @@
 const Trie = require('../Trie');
 
-describe('Trie.addWord', () => {
-    let trie;
+describe('Trie Module test', () => {
 
-    beforeEach(() => {
-        trie = new Trie();
+    // addWord
+    describe('Trie.addWord', () => {
+        let trie;
+
+        beforeEach(() => {
+            trie = new Trie();
+        });
+
+        test('should add a valid word', () => {
+            expect(trie.addWord('hello')).toBe(true);
+            expect(trie.findWord('hel')).toBe(true);
+        });
+
+        test('should not add an invalid word', () => {
+            expect(trie.addWord('')).toBe(false);
+            expect(trie.addWord(null)).toBe(false);
+            expect(trie.addWord(123)).toBe(false);
+        });
     });
 
-    test('should add a valid word', () => {
-        expect(trie.addWord('hello')).toBe(true);
+    // findWord
+    describe('Trie.findWord', () => {
+        let trie;
+
+        beforeEach(() => {
+            trie = new Trie();
+        });
+
+        test('should return true for partial words', () => {
+            trie.addWord('hello');
+            expect(trie.findWord('hel')).toBe(true);
+        });
+
+        test('should return false for non-string input', () => {
+            expect(trie.findWord(123)).toBe(false);
+            expect(trie.findWord(null)).toBe(false);
+        });
     });
 
-    test('should not add an invalid word', () => {
-        expect(trie.addWord('')).toBe(false);
-        expect(trie.addWord(null)).toBe(false);
-        expect(trie.addWord(123)).toBe(false);
-    });
 });
