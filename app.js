389253a3963ca40d3527866d13697b5a66d524bc
itaiglikman <itaig1998@gmail.com>
2025-07-23 16:10:51 +0300

    feat: app.js implementaion

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const Trie = require("./AutoCompleteModels/Trie");
-const TrieNode = require("./AutoCompleteModels/TrieNode");
+const printCommands = require("./Commands/commandsHandler");
+const promptSync = require('prompt-sync');
+const commandsService = require("./Services/commandsService");
 
-let trie = new Trie();
+const prompt = promptSync();
 
-
-function printTrie(node, indent = '') {
-    console.log(`${indent}${node.value}${node.endOfWord ? ' *' : ''}`);
-    for (const key in node.children) {
-        printTrie(node.children[key], indent + '  ');
+let [action, data] = commandsService.handleFirstCommand();
+const trie = new Trie();
+while (action !== 'exit') {
+    switch (action) {
+        case 'add':
+            commandsService.handleAdd(data, trie);
+            break;
+        case 'find':
+            commandsService.handleFind(data, trie);
+            break;
+        case 'complete':
+            commandsService.handleComplete(data, trie);
+            break;
+        case 'help':
+            printCommands.help()
+            break;
+        default:
+            printCommands.help();
+            break;
     }
+    console.dir(trie, { depth: null });
+    [action, data] = commandsService.handleNewCommand();
 }
 
-trie.addWord('hello');
-trie.addWord('hello');
-printTrie(trie.root);
-console.log(trie.findWord('elL'));
+printCommands.exit();
